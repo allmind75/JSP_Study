4b2f4d829dfa74ddd175804eef2b9ec443176a9a
Jang <allmind75@gmail.com>
2017-07-11 14:24:17 +0900

    request, session을 이용한 값 전달

--- ServletTest/WebContent/login_ok_request.jsp
+++ ServletTest/WebContent/login_ok_request.jsp
@@ -7,5 +7,12 @@
 </head>
 <body>
 	<h1>로그인 성공</h1>
+	<%
+		request.setCharacterEncoding("UTF-8");
+		String id = (String) request.getAttribute("UserID");
+	%>
+	<%=id %> 님이 로그인 성공<br>
+	request를 이용해서 값 전달
+	<a href="login_ok_session.jsp">menu</a>
 </body>
 </html>
--- ServletTest/src/test/Controller.java
+++ ServletTest/src/test/Controller.java
@@ -3,11 +3,13 @@ package test;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import member.MemberDAO;
 import member.MemberDTO;
@@ -38,10 +40,15 @@ public class Controller extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		// response.getWriter().append("Served at:
-		// ").append(request.getContextPath());
 
+		//utf-8
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		
+		//session
+		HttpSession session = request.getSession();
+		
+		
 		String id = request.getParameter("id");
 		String pw = request.getParameter("pw");
 
@@ -60,7 +67,24 @@ public class Controller extends HttpServlet {
 		try {
 			if(dao.login(dto)) {
 				System.out.println("로그인성공");
-				response.sendRedirect("login_ok.jsp");
+				
+				//request 내장 객체를 이용해서 다른 jsp 페이지로 값 전달
+				//새로운 페이지로 넘어갈때마다 새로운 request 객체 생성
+				request.setAttribute("UserID", id);
+				response.setContentType("text/html; charset=UTF-8");
+				
+				
+				//session 객체를 이용해서 값을 전달 - 페이지간 session 공유
+				session.setAttribute("UserID", id);
+				
+				
+				//request에 값 저장해서 다른 페이지로 넘어갈 때
+				//RequestDispatcher DS = request.getRequestDispatcher("login_ok_request.jsp");
+				//DS.forward(request, response);
+				
+				
+				//session에 저장한 값은 유지, request는 안됨
+				response.sendRedirect("login_ok_request.jsp");		
 			} else {
 				System.out.println("로그인실패");
 				response.sendRedirect("login_fail.jsp");
